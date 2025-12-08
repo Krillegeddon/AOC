3ca7bb89b077e077316895a7d0283aa79e5cbcb5
Christian Jansson <christian.e.jansson@gmail.com>
2025-12-09 00:20:36 +0100

    Done day 8 part 1

--- Advent2025/Day08/Logic.cs
+++ Advent2025/Day08/Logic.cs
@@ -1,5 +1,11 @@
 ﻿namespace Advent2025.Day08;
 
+
+public class Circuit
+{
+    public List<int> JuncionBoxIndexes { get; set; }
+}
+
 public class Logic
 {
     private Model _model;
@@ -8,9 +14,112 @@ public class Logic
         _model = model;
     }
 
+    private List<Circuit> _circuits = new List<Circuit>();
+
+    private Dictionary<string, bool> _connectionDict = new Dictionary<string, bool>();
+
+    private Dictionary<int, Circuit> _junctionBoxIndexToCircuit = new Dictionary<int, Circuit>();
+
+    private void ConnectTwoShortest()
+    {
+        var minDist = double.MaxValue;
+        Point p1 = null;
+        Point p2 = null;
+        var i1 = -1;
+        var i2 = -1;
+        for (int i = 0; i < _model.JunctionBoxes.Count; i++)
+        {
+            for (int j = i + 1; j < _model.JunctionBoxes.Count; j++)
+            {
+                var key = $"{i}_{j}";
+
+                if (_connectionDict.ContainsKey(key))
+                {
+                    // We have already connected these two guys
+                    continue;
+                }
+
+                var dist = Point.CalculateDistance(_model.JunctionBoxes[i], _model.JunctionBoxes[j]);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    p1 = _model.JunctionBoxes[i];
+                    p2 = _model.JunctionBoxes[j];
+                    i1 = i;
+                    i2 = j;
+                }
+            }
+        }
+
+        // See if any of them has a circuit
+        Circuit circ1 = null;
+        if (_junctionBoxIndexToCircuit.ContainsKey(i1))
+        {
+            circ1 = _junctionBoxIndexToCircuit[i1];
+        }
+        Circuit circ2 = null;
+        if (_junctionBoxIndexToCircuit.ContainsKey(i2))
+        {
+            circ2 = _junctionBoxIndexToCircuit[i2];
+        }
+
+        if (circ1 == null && circ2 == null)
+        {
+            // Both are new, create a new circuit
+            circ1 = new Circuit { JuncionBoxIndexes = new List<int> { i1, i2 } };
+            _junctionBoxIndexToCircuit.Add(i1, circ1);
+            _junctionBoxIndexToCircuit.Add(i2, circ1);
+            _circuits.Add(circ1);
+        }
+        else if (circ1 != null && circ2 == null)
+        {
+            // Only circ1 exists, add i2 to it
+            circ1.JuncionBoxIndexes.Add(i2);
+            _junctionBoxIndexToCircuit.Add(i2, circ1);
+        }
+        else if (circ1 == null && circ2 != null)
+        {
+            // Only circ2 exists, add i1 to it
+            circ2.JuncionBoxIndexes.Add(i1);
+            _junctionBoxIndexToCircuit.Add(i1, circ2);
+        }
+        else
+        {
+            if (circ1 == circ2)
+            {
+                // Both are the same circuit, nothing to do
+                //return;
+            }
+            else
+            {
+
+                // Both exist, merge them
+                circ1.JuncionBoxIndexes.AddRange(circ2.JuncionBoxIndexes);
+                _circuits.Remove(circ2);
+                foreach (var id in (circ1.JuncionBoxIndexes))
+                {
+                    _junctionBoxIndexToCircuit[id] = circ1;
+                }
+            }
+        }
+
+        // Make a connection
+        _connectionDict.Add($"{i1}_{i2}", true);
+    }
+
+
     public string Run()
     {
         long sum = 0;
+
+        while (_connectionDict.Count < 1000)
+        {
+            ConnectTwoShortest();
+        }
+
+        var circs = _circuits.Select(p=>p.JuncionBoxIndexes.Count).OrderByDescending(p => p).Take(3).ToList();
+
+        sum = circs[0] * circs[1] * circs[2];
 
         return sum.ToString();
     }
--- Advent2025/Day08/Model.cs
+++ Advent2025/Day08/Model.cs
@@ -1,14 +1,41 @@
 ﻿namespace Advent2025.Day08;
 
+public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public override string ToString()
+    {
+        return $"({X},{Y},{Z})";
+    }
+
+    public static double CalculateDistance(Point p1, Point p2)
+    {
+        double distance = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) +
+                                     Math.Pow(p2.Y - p1.Y, 2) +
+                                     Math.Pow(p2.Z - p1.Z, 2));
+        return distance;
+    }
+}
+
 public class Model
 {
-    public required List<object> Obj { get; set; }
+    public required List<Point> JunctionBoxes { get; set; }
 
     public static Model Parse()
     {
         var retObj = new Model
         {
-            Obj = new List<object>()
+            JunctionBoxes = new List<Point>()
         };
 
         foreach (var lx in Data.InputData.Split("\n"))
@@ -17,6 +44,12 @@ public class Model
 
             if (string.IsNullOrEmpty(l))
                 continue;
+
+            var parts = l.Split(',');
+            var point = new Point(double.Parse(parts[0]),
+                                  double.Parse(parts[1]),
+                                  double.Parse(parts[2]));
+            retObj.JunctionBoxes.Add(point);
         }
 
         return retObj;
